a8fcbcda33f5657e049e02e8ff82d1b17495ec0f
Sergio Bermejo <sergio.beay@gmail.com>
2015-07-29 23:54:17 +0200

    Multiples preguntas y Autoload

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-var quizController = require('../controllers/quiz_controller')
+var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -12,7 +12,12 @@ router.get('/author', function(req,res){
 	res.render('author');
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load); // autoload :quizId
+
+// Definicion de rutas de /quizes
+router.get('/quizes', quizController.index);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 module.exports = router;
