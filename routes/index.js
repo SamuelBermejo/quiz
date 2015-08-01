4b9316582f95aea30e008da13a4a89d48c5a6dcc
Sergio Bermejo <sergio.beay@gmail.com>
2015-08-01 20:12:14 +0200

    Moderacion de comentarios

--- routes/index.js
+++ routes/index.js
@@ -16,6 +16,7 @@ router.get('/author', function(req,res){
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load); // autoload :quizId
+router.param('commentId', commentController.load); // autoload :commentId
 
 // Definicion de rutas de session
 router.get('/login', sessionController.new); //formulario login
@@ -34,5 +35,6 @@ router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizCont
 
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
 module.exports = router;
